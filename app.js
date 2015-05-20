9a5bc599379bee1eab48ca71364e7fb0e416306b
Xunrong Li <lixunrong@gmail.com>
2015-05-20 15:20:21 -0400

    complete comments

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ function($stateProvider, $urlRouterProvider) {
 		controller: 'MainCtrl'
 	});
 
+	$stateProvider.state('posts', {
+		url: '/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'
+	});
+
 	$urlRouterProvider.otherwise('home');
 }])
 .factory('posts', [function(){
 	var o = {
-		posts: [{title: "hello", link: ',', upvotes: 0}]
+		posts: []
 	};
 	return o;
 }])
@@ -28,13 +34,36 @@ function($scope, posts){
     	$scope.posts.push({
     		title: $scope.title,
     		link: $scope.link,
-    		upvotes: 0
+    		upvotes: 0,
+			comments: []
     	});
     	$scope.title = '';
     	$scope.link = '';
-	}
+	};
+
 
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
 	}
-}]);
+}])
+.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+
+		$scope.addComment = function() {
+			if ($scope.body === '') { return; };
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+		};
+
+
+
+	}
+]);
