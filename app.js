5962bda7d9dff54f43f037328e6dfcce2025cf57
Xunrong Li <lixunrong@gmail.com>
2015-05-20 13:34:42 -0400

    use factory

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-var app = angular.module('flapperNews', []);
-
-app.controller('MainCtrl', [
+var app = angular.module('flapperNews', [])
+.factory('posts', [function(){
+	var o = {
+		posts: [{title: "hello", link: ',', upvotes: 0}]
+	};
+	return o;
+}])
+.controller('MainCtrl', [
 '$scope',
-function($scope){
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
-    ];
-  ];
+'posts',
+function($scope, posts){
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function() {
+    	if ($scope.title === '') { return; };
+    	$scope.posts.push({
+    		title: $scope.title,
+    		link: $scope.link,
+    		upvotes: 0
+    	});
+    	$scope.title = '';
+    	$scope.link = '';
+	}
+
+	$scope.incrementUpvotes = function(post) {
+		post.upvotes += 1;
+	}
 }]);
